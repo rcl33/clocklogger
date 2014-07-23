5a8d40fe4d89a93a0eaea74b9493195279a68e3c
Rick Lupton <r.lupton@gmail.com>
2014-07-23 09:28:27 +0100

    Test TextFileWriter with actual files (not mocks)

--- tests/output/test_textfile.py
+++ tests/output/test_textfile.py
@@ -13,86 +13,69 @@ from clocklogger.output.textfile import TextFileWriter
 class TextFileWriterTestCase(unittest.TestCase):
     def setUp(self):
         self.path = mkdtemp()
+        self.writer = TextFileWriter(self.path, ['time', 'a', 'b'])
 
     def tearDown(self):
         shutil.rmtree(self.path)
 
     def test_opens_and_closes_file(self):
-        writer = TextFileWriter(self.path, ['a', 'b'])
         data = {'time': datetime(2014, 2, 3, 10, 30, 23),
                 'a': 2.3, 'b': 4.3, 'c': 3.4}
 
-        # Patch with create=True to overwrite buildin open()
-        with patch('clocklogger.output.textfile.open', mock_open(), create=True) as m:
-            writer.write(data)
-
         # Check correct filename was opened
         expected_path = os.path.join(self.path, '2014/02/clock-2014-02-03.txt')
-        m.assert_called_once_with(expected_path, 'at')
+        self.assertFalse(os.path.exists(expected_path))
+        self.writer.write(data)
+        self.assertTrue(os.path.exists(expected_path))
+        self.assertFalse(self.writer.file.closed)
 
         # Check file is closed when writer is deleted
-        f = writer.file
-        del writer
-        f.close.assert_called_once_with()
+        f = self.writer.file
+        del self.writer
+        self.assertTrue(f.closed)
 
     def test_changes_file_at_midnight(self):
-        writer = TextFileWriter(self.path, ['a', 'b'])
         data = {'time': datetime(2014, 2, 3, 23, 59, 0),
                 'a': 2.3, 'b': 4.3, 'c': 3.4}
 
-        # Patch with create=True to overwrite buildin open()
-        with patch('clocklogger.output.textfile.open', mock_open(), create=True) as m:
-            def save_file_name(fn, *args):
-                mock_file = MagicMock()
-                mock_file.name = fn
-                return mock_file
-            m.side_effect = save_file_name
+        # First write - old file
+        expected1 = os.path.join(self.path, '2014/02/clock-2014-02-03.txt')
+        self.assertFalse(os.path.exists(expected1))
+        self.writer.write(data)
+        self.assertTrue(os.path.exists(expected1))
+        self.assertFalse(self.writer.file.closed)
 
-            # First write - old file
-            writer.write(data)
-            f = writer.file
-            expected1 = os.path.join(self.path, '2014/02/clock-2014-02-03.txt')
-            m.assert_called_once_with(expected1, 'at')
+        # Next point - still old file
+        data['time'] += timedelta(seconds=30)
+        file1 = self.writer.file
+        self.writer.write(data)
+        self.assertEqual(self.writer.file, file1)
 
-            # Next point - still old file
-            data['time'] += timedelta(seconds=30)
-            writer.write(data)
-            self.assertEqual(m.call_count, 1)
-            self.assertEqual(f.close.call_count, 0)
+        # Next point - midnight - new file
+        data['time'] += timedelta(seconds=30)
+        expected2 = os.path.join(self.path, '2014/02/clock-2014-02-04.txt')
+        self.assertFalse(os.path.exists(expected2))
+        self.writer.write(data)
+        self.assertTrue(os.path.exists(expected2))
+        self.assertNotEqual(self.writer.file, file1)
+        self.assertTrue(file1.closed)
+        self.assertFalse(self.writer.file.closed)
 
-            # Next point - midnight - new file
-            data['time'] += timedelta(seconds=30)
-            expected2 = os.path.join(self.path, '2014/02/clock-2014-02-04.txt')
-            writer.write(data)
-            self.assertEqual(m.call_count, 2)
-            m.assert_has_calls([call(expected1, 'at'),
-                                call(expected2, 'at')])
-            self.assertEqual(f.close.call_count, 1)
-
-            # Next point - still new file
-            data['time'] += timedelta(seconds=30)
-            writer.write(data)
-            self.assertEqual(m.call_count, 2)
-            self.assertEqual(f.close.call_count, 1)
+        # Next point - still new file
+        data['time'] += timedelta(seconds=30)
+        file2 = self.writer.file
+        self.writer.write(data)
+        self.assertEqual(self.writer.file, file2)
 
     def test_output(self):
-        writer = TextFileWriter(self.path, ['time', 'a', 'b'])
         data1 = {'time': datetime(1970, 1, 1, 0, 0, 0),
                  'a': 2.3, 'b': 4.328392012, 'c': 3.4}
         data2 = {'time': datetime(1970, 1, 1, 13, 0, 4),
                  'a': np.float64(4.1), 'b': 302.1, 'c': 0.1}
 
-        # Patch with create=True to overwrite buildin open()
-        with patch('clocklogger.output.textfile.open', mock_open(), create=True) as m:
-            def save_file_name(fn, *args):
-                mock_file = MagicMock()
-                mock_file.name = fn
-                return mock_file
-            m.side_effect = save_file_name
-
-            # First write - old file
-            writer.write(data1)
-            writer.write(data2)
+        # First write - old file
+        self.writer.write(data1)
+        self.writer.write(data2)
 
         # Check output is as expected
         expected_output = [
@@ -100,6 +83,5 @@ class TextFileWriterTestCase(unittest.TestCase):
             "0	2.300000	4.328392\n",
             "46804	4.100000	302.100000\n"
         ]
-        writer.file.write.assert_has_calls([
-            call(line) for line in expected_output
-        ])
+        with open(os.path.join(self.path, '1970/01/clock-1970-01-01.txt')) as f:
+            self.assertEqual(f.readlines(), expected_output)
